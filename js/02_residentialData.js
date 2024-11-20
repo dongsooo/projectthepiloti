46d76b61ff3c595699ef3bf1e7abcaa8a4951eb8
dongsu <ehdtn8910@naver.com>
2024-11-21 08:18:33 +0900

    수정됨

--- js/02_residentialData.js
+++ js/02_residentialData.js
@@ -1,10 +1,10 @@
 // 리스트 생성 관련
 export const imgData = [
   {
-    src: "../img/2020/APT_KUMHO_DAEWOO/kumho8.jpg",
-    text: "광주 북구",
-    info: "금호 대우 아파트",
-    link: "https://www.google.co.https://example.com/link3/?hl=ko",
+    src: "img/2020/APT_KUMHO_DAEWOO/kumho8.jpg",
+    text: "광주 서구",
+    info: "KUMHO DAEWOO APT",
+    link: "detailPage/AptKumhoDaewoo24py.html",
     customClass: "apartment",
   },
 ];
